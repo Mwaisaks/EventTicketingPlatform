4dbea2dc5692162efa16619b8dbdb30557b5db21
Phenny Mwaisaka <mwaisakaphenny@gmail.com>
2025-06-30 07:16:53 +0300

    Completed User Provisioning module by enabling JPA audit

---
+++ src/main/java/com/devtiro/EventTicketingPlatform/config/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package com.devtiro.EventTicketingPlatform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+}
